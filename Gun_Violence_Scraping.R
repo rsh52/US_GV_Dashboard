a2e70c8b28cd28c8cf257f575093e01cfde0be96
Rich <richardshanna91@gmail.com>
2020-12-17 08:56:58 -0500

    Fixed Year df Issue :calendar:
    There was an issue where the dataframes for years 2016 - 2020 were not rendering due to a website change. The unlist to df creation was offset and causing an error so the dashboard would not load

--- Gun_Violence_Scraping.R
+++ Gun_Violence_Scraping.R
@@ -75,14 +75,16 @@ df2016 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shootin
                  })
 df2016 <- unlist(df2016)
 df2016 <- data.frame(
-  #incident_id = df2016[seq(from = 1, to = length(df2016), by = 8)],
-  incident_date = df2016[seq(from = 1, to = length(df2016), by = 7)],
-  state = df2016[seq(from = 2, to = length(df2016), by = 7)],
-  city_county = df2016[seq(from = 3, to = length(df2016), by = 7)],
-  address = df2016[seq(from = 4, to = length(df2016), by = 7)],
-  no.killed = df2016[seq(from = 5, to = length(df2016), by = 7)],
-  no.injured = df2016[seq(from = 6, to = length(df2016), by = 7)]
+  incident_id = df2016[seq(from = 1, to = length(df2016), by = 8)],
+  incident_date = df2016[seq(from = 1, to = length(df2016), by = 8)],
+  state = df2016[seq(from = 2, to = length(df2016), by = 8)],
+  city_county = df2016[seq(from = 3, to = length(df2016), by = 8)],
+  address = df2016[seq(from = 4, to = length(df2016), by = 8)],
+  no.killed = df2016[seq(from = 5, to = length(df2016), by = 8)],
+  no.injured = df2016[seq(from = 6, to = length(df2016), by = 8)]
 )
+
+df2016[1] <- NULL
 
 # 2017 Data ====================================================================
 df2017 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shooting?year=2017&page=", 0:14),
@@ -93,14 +95,16 @@ df2017 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shootin
                  })
 df2017 <- unlist(df2017)
 df2017 <- data.frame(
-  #incident_id = df2017[seq(from = 1, to = length(df2017), by = 8)],
-  incident_date = df2017[seq(from = 1, to = length(df2017), by = 7)],
-  state = df2017[seq(from = 2, to = length(df2017), by = 7)],
-  city_county = df2017[seq(from = 3, to = length(df2017), by = 7)],
-  address = df2017[seq(from = 4, to = length(df2017), by = 7)],
-  no.killed = df2017[seq(from = 5, to = length(df2017), by = 7)],
-  no.injured = df2017[seq(from = 6, to = length(df2017), by = 7)]
+  incident_id = df2017[seq(from = 1, to = length(df2017), by = 8)],
+  incident_date = df2017[seq(from = 1, to = length(df2017), by = 8)],
+  state = df2017[seq(from = 2, to = length(df2017), by = 8)],
+  city_county = df2017[seq(from = 3, to = length(df2017), by = 8)],
+  address = df2017[seq(from = 4, to = length(df2017), by = 8)],
+  no.killed = df2017[seq(from = 5, to = length(df2017), by = 8)],
+  no.injured = df2017[seq(from = 6, to = length(df2017), by = 8)]
 )
+
+df2017[1] <- NULL
 
 # 2018 Data ====================================================================
 df2018 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shooting?year=2018&page=", 0:14),
@@ -111,14 +115,16 @@ df2018 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shootin
                  })
 df2018 <- unlist(df2018)
 df2018 <- data.frame(
-  #incident_id = df2018[seq(from = 1, to = length(df2018), by = 8)],
-  incident_date = df2018[seq(from = 1, to = length(df2018), by = 7)],
-  state = df2018[seq(from = 2, to = length(df2018), by = 7)],
-  city_county = df2018[seq(from = 3, to = length(df2018), by = 7)],
-  address = df2018[seq(from = 4, to = length(df2018), by = 7)],
-  no.killed = df2018[seq(from = 5, to = length(df2018), by = 7)],
-  no.injured = df2018[seq(from = 6, to = length(df2018), by = 7)]
+  incident_id = df2018[seq(from = 1, to = length(df2018), by = 8)],
+  incident_date = df2018[seq(from = 1, to = length(df2018), by = 8)],
+  state = df2018[seq(from = 2, to = length(df2018), by = 8)],
+  city_county = df2018[seq(from = 3, to = length(df2018), by = 8)],
+  address = df2018[seq(from = 4, to = length(df2018), by = 8)],
+  no.killed = df2018[seq(from = 5, to = length(df2018), by = 8)],
+  no.injured = df2018[seq(from = 6, to = length(df2018), by = 8)]
 )
+
+df2018[1] <- NULL
 
 # 2019 Data ====================================================================
 df2019 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shooting?year=2019&page=", 0:17),
@@ -129,14 +135,16 @@ df2019 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shootin
                  })
 df2019 <- unlist(df2019)
 df2019 <- data.frame(
-  #incident_id = df2019[seq(from = 1, to = length(df2019), by = 8)],
-  incident_date = df2019[seq(from = 1, to = length(df2019), by = 7)],
-  state = df2019[seq(from = 2, to = length(df2019), by = 7)],
-  city_county = df2019[seq(from = 3, to = length(df2019), by = 7)],
-  address = df2019[seq(from = 4, to = length(df2019), by = 7)],
-  no.killed = df2019[seq(from = 5, to = length(df2019), by = 7)],
-  no.injured = df2019[seq(from = 6, to = length(df2019), by = 7)]
+  incident_id = df2019[seq(from = 1, to = length(df2019), by = 8)],
+  incident_date = df2019[seq(from = 1, to = length(df2019), by = 8)],
+  state = df2019[seq(from = 2, to = length(df2019), by = 8)],
+  city_county = df2019[seq(from = 3, to = length(df2019), by = 8)],
+  address = df2019[seq(from = 4, to = length(df2019), by = 8)],
+  no.killed = df2019[seq(from = 5, to = length(df2019), by = 8)],
+  no.injured = df2019[seq(from = 6, to = length(df2019), by = 8)]
 )
+
+df2019[1] <- NULL
 
 # 2020 Data ====================================================================
 df2020 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shooting?year=2020&page=", 0:50),
@@ -147,16 +155,17 @@ df2020 <- lapply(paste0("https://www.gunviolencearchive.org/reports/mass-shootin
                  })
 df2020 <- unlist(df2020)
 df2020 <- data.frame(
-  #incident_id = df2020[seq(from = 1, to = length(df2020), by = 8)],
-  incident_date = df2020[seq(from = 1, to = length(df2020), by = 7)],
-  state = df2020[seq(from = 2, to = length(df2020), by = 7)],
-  city_county = df2020[seq(from = 3, to = length(df2020), by = 7)],
-  address = df2020[seq(from = 4, to = length(df2020), by = 7)],
-  no.killed = df2020[seq(from = 5, to = length(df2020), by = 7)],
-  no.injured = df2020[seq(from = 6, to = length(df2020), by = 7)]
+  incident_id = df2020[seq(from = 1, to = length(df2020), by = 8)],
+  incident_date = df2020[seq(from = 1, to = length(df2020), by = 8)],
+  state = df2020[seq(from = 2, to = length(df2020), by = 8)],
+  city_county = df2020[seq(from = 3, to = length(df2020), by = 8)],
+  address = df2020[seq(from = 4, to = length(df2020), by = 8)],
+  no.killed = df2020[seq(from = 5, to = length(df2020), by = 8)],
+  no.injured = df2020[seq(from = 6, to = length(df2020), by = 8)]
 ) %>% 
   distinct()
 
+df2020[1] <- NULL
 
 # Combined GV Dataset ==========================================================
 
